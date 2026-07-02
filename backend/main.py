799b6aa54629d874367bb578aa3374da89c7fbc6
PriyamJChakrabarty <144861619+PriyamJChakrabarty@users.noreply.github.com>
2026-07-03 01:04:16 +0530

    Removing Transformer
    Removing Transformer
    
    Signed-off-by: PriyamJChakrabarty <144861619+PriyamJChakrabarty@users.noreply.github.com>

--- backend/main.py
+++ backend/main.py
@@ -7,11 +7,9 @@ from routes.transcribe import router as transcribe_router
 from routes.extract import router as extract_router
 from routes.actions import router as actions_router
 from api.chatbot import router as chatbot_router
-from routes.ocr_upload import router as ocr_upload_router
 from routes.suggest_and_implement_changes import router as suggest_changes_router
 from routes.suggest_and_implement_changes import router2 as apply_changes_router
 from routes.query_docs import router as query_doc_router
-from routes.map_clauses import router as map_clauses_router
 
 
 app = FastAPI()
@@ -28,11 +26,9 @@ app.include_router(transcribe_router)
 app.include_router(extract_router, prefix="/api")
 app.include_router(actions_router, prefix="/api")
 app.include_router(chatbot_router)
-app.include_router(ocr_upload_router, prefix="/api")
 app.include_router(suggest_changes_router, prefix="/api")
 app.include_router(apply_changes_router, prefix="/api")
 app.include_router(query_doc_router,prefix="/api")
-app.include_router(map_clauses_router, prefix="/api")
 
 @app.get("/")
 async def root():
